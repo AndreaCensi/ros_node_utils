cc6efb14cee98287b13de6e5470a0f537c10dc7a
Andrea Censi <andrea@cds.caltech.edu>
2013-04-29 15:17:54 -0700

    debug message

--- src/ros_node_utils/nodes/ros_node.py
+++ src/ros_node_utils/nodes/ros_node.py
@@ -1,4 +1,3 @@
-import rospy
 
 
 class ROSNode():
@@ -11,8 +10,13 @@ class ROSNode():
     # Logging shortcuts
     
     def info(self, s):
+        import rospy
         rospy.loginfo("%s%s" % (self.prefix, s))  # @UndefinedVariable
 
     def error(self, s):
+        import rospy
         rospy.logerr("%s%s" % (self.prefix, s))  # @UndefinedVariable
 
+    def debug(self, s):
+        import rospy
+        rospy.logdebug("%s%s" % (self.prefix, s))  # @UndefinedVariable
