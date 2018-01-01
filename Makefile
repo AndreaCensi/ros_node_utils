480836fc6753930edcf8c95f7555b6f3953bfd35
Andrea Censi <acensi@ethz.ch>
2018-01-01 13:04:29 +0100

    bump

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
 package=ros_node_utils
 
 include pypackage.mk
+
+
+
+bump-upload:
+    bumpversion patch
+    git push --tags
+    git push --all
+    rm -f dist/*
+    python setup.py sdist
+    twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 import os
 from setuptools import setup, find_packages
 
-version = "1.1"
+
+def get_version(filename):
+    import ast
+    version = None
+    with file(filename) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError('No version found in %r.' % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+version = get_version(filename='src/ros_node_utils/__init__.py')
+
 
 description = """
 
